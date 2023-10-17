e3e4e618b47b83286cb52da9f46916cde64239a8
Michelle Primiani <primianimichelle@gmail.com>
2023-10-17 11:11:06 -0400

    Added url_params

--- json-server.py
+++ json-server.py
@@ -18,6 +18,7 @@ class JSONServer(HandleRequests):
 
         response_body = ""
         url = self.parse_url(self.path)
+        url_params = url.get("query_params", {})  # Get the query parameters
 
         if url["requested_resource"] == "docks":
             if url["pk"] != 0:
@@ -37,7 +38,7 @@ class JSONServer(HandleRequests):
 
         elif url["requested_resource"] == "ships":
             if url["pk"] != 0:
-                response_body = retrieve_ship(url["pk"])
+                response_body = retrieve_ship(url["pk"], url_params)
                 return self.response(response_body, status.HTTP_200_SUCCESS.value)
 
             response_body = list_ships()
--- views/ship_view.py
+++ views/ship_view.py
@@ -37,26 +37,63 @@ def delete_ship(pk):
     return True if number_of_rows_deleted > 0 else False
 
 
-def list_ships(url):
+def list_ships(url_params=None):
     # Open a connection to the database
     with sqlite3.connect("./shipping.db") as conn:
         conn.row_factory = sqlite3.Row
         db_cursor = conn.cursor()
 
-        # Write the SQL query to get the information you want
-        db_cursor.execute("""
-        SELECT
-            s.id,
-            s.name,
-            s.hauler_id
-        FROM Ship s
-        """)
+        # Check if the '_expand' parameter exists in the URL dictionary
+        if '_expand' in url_params:
+            # Handle the case when _expand exists in the URL
+            # Modify your query or processing here as needed
+            db_cursor.execute("""
+            SELECT
+                s.id,
+                s.name,
+                s.hauler_id,
+                h.id haulerId,
+                h.name haulerName,
+                h.dock_id
+            FROM Ship s
+            JOIN Hauler h 
+                ON h.id = s.hauler_id
+            """)
+        else:
+            # Write the default SQL query to get the information you want
+            db_cursor.execute("""
+            SELECT
+                s.id,
+                s.name,
+                s.hauler_id
+            FROM Ship s
+            """)
+
         query_results = db_cursor.fetchall()
 
         # Initialize an empty list and then add each dictionary to it
         ships = []
         for row in query_results:
-            ships.append(dict(row))
+            # Custom response building
+            # if '_expand' in url_params:
+            # Build a hauler dictionary with the correct keys and values
+            hauler = {
+                "id": row['haulerId'],
+                "name": row['haulerName'],
+                "dock_id": row["dock_id"]
+            }
+            # Build a ship dictionary that includes the hauler dictionary as a nested dictionary
+            ship = {
+                "id": row['id'],
+                "name": row['name'],
+                "hauler_id": row["hauler_id"],
+                "hauler": hauler
+            }
+         # Append the new ship dictionary to the list of ships
+            ships.append(ship)
+            # else:
+            # If not expanding, append the row as is
+            # ships.append(dict(row))
 
         # Serialize Python list to JSON encoded string
         serialized_ships = json.dumps(ships)
@@ -64,25 +101,64 @@ def list_ships(url):
     return serialized_ships
 
 
-def retrieve_ship(pk):
+def retrieve_ship(pk, url_params=None):
     # Open a connection to the database
     with sqlite3.connect("./shipping.db") as conn:
         conn.row_factory = sqlite3.Row
         db_cursor = conn.cursor()
 
-        # Write the SQL query to get the information you want
-        db_cursor.execute("""
-        SELECT
-            s.id,
-            s.name,
-            s.hauler_id
-        FROM Ship s
-        WHERE s.id = ?
-        """, (pk,))
+        # Check if the '_expand' parameter exists in the URL dictionary
+        if '_expand' in url_params:
+            # Handle the case when _expand exists in the URL
+            db_cursor.execute("""
+            SELECT
+                s.id,
+                s.name,
+                s.hauler_id,
+                h.id haulerId,
+                h.name haulerName,
+                h.dock_id
+            FROM Ship s
+            JOIN Hauler h ON s.hauler_id = h.id
+            WHERE s.id = ?
+            """, (pk,))
+        else:
+            # Write the default SQL query to get the information you want
+            db_cursor.execute("""
+            SELECT
+                s.id,
+                s.name,
+                s.hauler_id
+            FROM Ship s
+            WHERE s.id = ?
+            """, (pk,))
+
         query_results = db_cursor.fetchone()
 
-        # Serialize Python list to JSON encoded string
-        dictionary_version_of_object = dict(query_results)
-        serialized_ship = json.dumps(dictionary_version_of_object)
+        if query_results:
+            if '_expand' in url_params:
+                # Build a hauler dictionary with the correct keys and values
+                hauler = {
+                    "id": query_results['haulerId'],
+                    "name": query_results['haulerName'],
+                    "dock_id": query_results["dock_id"]
+                }
+
+                # Build a ship dictionary that includes the hauler dictionary as a nested dictionary
+                ship = {
+                    "id": query_results['id'],
+                    "name": query_results['name'],
+                    "hauler_id": query_results["hauler_id"],
+                    "hauler": hauler
+                }
+
+                # Serialize the ship dictionary to JSON encoded string
+                serialized_ship = json.dumps(ship)
+            else:
+                # Serialize the query results as is to JSON encoded string
+                serialized_ship = json.dumps(dict(query_results))
+        else:
+            # Return a 404 response if the ship with the given ID is not found
+            return None
 
     return serialized_ship
